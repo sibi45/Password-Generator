00f72b81b632847b440f8cfc0ea60000c42815d7
sibi45 <sibisaranai@gmail.com>
2024-11-21 22:47:18 +0530

    password generator

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import { PasswordGeneraator } from './password generator'
+
+function App() {
+ 
+
+  return (
+    <>
+      <PasswordGeneraator/>
+    </>
+  )
+}
+
+export default App
